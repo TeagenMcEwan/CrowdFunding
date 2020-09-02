edc2a4100675da9aaef17ce5041e5c0674d42ad8
TeagenMcEwan <67173830+TeagenMcEwan@users.noreply.github.com>
2020-09-02 17:53:00 +0800

    Updated admin access permissions

--- wasports/projects/admin.py
+++ wasports/projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Project, Pledge
+#Add other fields) above after pledge
 
 # Register your models here.
+
+#Replace 
+admin.site.register(Project)
+admin.site.register(Pledge)
